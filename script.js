ab4f1d227b63152a92dca9281ad58a017c83cdf9
dakzkd <damir.kadyr@gmail.com>
2021-07-06 12:49:18 +0600

    kata #2

--- script.js
+++ script.js
@@ -35,4 +35,28 @@
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// function oddOrEven(array) {
+    //enter code here
+    // if (array.length === 0) return 'even'
+    // else {
+
+    //     const sum = array.reduce((sum,el)=>sum+el );
+    //     return sum%2 === 0 ? 'even' : 'odd';
+    // }
+    //  Calling reduce() on an empty array without an initialValue will throw a TypeError
+   
+//         return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+//  }
+
+
+// console.log(oddOrEven([0, 1, 4]));
+
+
 
